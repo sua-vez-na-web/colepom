7b1d4457de55d4048f101fb95e6abcea2c735981
Matthaus Nawan <matthausnawan@gmail.com>
2020-07-10 01:20:51 -0300

    ajuste na acl

--- resources/views/admin/pages/affiliates/index.blade.php
+++ resources/views/admin/pages/affiliates/index.blade.php
@@ -19,10 +19,8 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="panel panel-default">
-                        <div class="panel-heading">
-                            @can('syndicates')
-                                <a href="{{route('affiliates.create')}}" class="btn btn-primary"><i class="fa fa-plus"></i> Novo Associado </a>
-                            @endcan
+                        <div class="panel-heading">                          
+                            <a href="{{route('affiliates.create')}}" class="btn btn-primary"><i class="fa fa-plus"></i> Novo Associado </a>                           
                         </div>
                         <!-- /.panel-heading -->
                         <div class="panel-body">
