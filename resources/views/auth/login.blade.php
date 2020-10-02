6eff5f0499a07b78b6927af9f3fe99c1c589a668
Matthaus Nawan <matthausnawan@gmail.com>
2020-10-02 00:34:31 -0300

    improovements layouts

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,14 +3,14 @@
 @section('content')
 <div class="container mt-5">
     <div class="row justify-content-center align-items-center">
-        <div class="col-md-10">
+        <div class="col-md-6">
             <div class="card-body">
                 <form method="POST" action="{{ route('login') }}">
                     @csrf
                     <div class="form-group row">
                         <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
 
-                        <div class="col-md-6">
+                        <div class="col-md-8">
                             <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
                             @if ($errors->has('email'))
@@ -24,7 +24,7 @@
                     <div class="form-group row">
                         <label for="password" class="col-md-4 col-form-label text-md-right">Senha</label>
 
-                        <div class="col-md-6">
+                        <div class="col-md-8">
                             <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                             @if ($errors->has('password'))
@@ -36,7 +36,7 @@
                     </div>
 
                     <div class="form-group row">
-                        <div class="col-md-6 offset-md-4">
+                        <div class="col-md-8 offset-4">
                             <button type="submit" class="btn btn-lg btn-block full_colored ">Entrar</button>
                             <!-- </div> -->
                         </div>
@@ -44,7 +44,7 @@
                 </form>
             </div>
         </div>
-        <div class="col-md-2">
+        <div class="col-md-6 justify-content-center d-flex align-items-center flex-column">
             <h4 class="text-center">Não tem uma conta?
             </h4>
             <a href="{{ route('affiliates.register') }}" class="btn btn-outline-dark">Cadastre-se aqui</a>
